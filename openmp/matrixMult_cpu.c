038cd0108d7bcb016dfecab13918e95c56fd3e1a
Andres Forero <afforeroc@unal.edu.co>
2019-07-21 17:33:34 -0500

    Pointers

--- openmp/matrixMult_cpu.c
+++ openmp/matrixMult_cpu.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
-// Global variables
-int A[1024][1024];
-int B[1024][1024];
-int C[1024][1024];
+int** createMatrix(int N){
+    int** M = (int **)malloc(N * sizeof(int *));
+    int i; 
+    for (i=0; i<N; i++) 
+        M[i] = (int *)malloc(N * sizeof(int));
+    return M;
+}
 
-void readMatrix(char *filename, int M[][1024], int N){
+void readMatrix(char* filename, int** M, int N){
     FILE *fstream = fopen(filename, "r");
     if(fstream == NULL){
         printf("\n file opening failed ");
@@ -28,7 +31,7 @@ void readMatrix(char *filename, int M[][1024], int N){
     }
 }
 
-void printMatrix(int M[][1024], int N){
+void printMatrix(int** M, int N){
     // print matrix for testing
     int i;
     int j;
@@ -41,25 +44,24 @@ void printMatrix(int M[][1024], int N){
     printf("\n");
 }
 
-void multiplyMatrix(int A[][1024], int B[][1024], int C[][1024]) 
-{ 
+void multiplyMatrix(int** A, int** B, int** C, int N, int ini, int fin){ 
     int i, j, k; 
-    for (i = 0; i < 1024; i++) { 
-        for (j = 0; j < 1024; j++) { 
+    for (i = ini; i < fin; i++) { 
+        for (j = 0; j < N; j++) { 
             C[i][j] = 0; 
-            for (k = 0; k < 1024; k++) 
+            for (k = 0; k < N; k++) 
                 C[i][j] += A[i][k]*B[k][j]; 
         } 
     } 
 }
 
-void writeMatrix(char *filename, int R[][1024], int N){
+void writeMatrix(char *filename, int** C, int N){
     FILE *fp;
     int i,j;
     fp=fopen(filename,"w+");
     for(i=0 ; i<N; i++){
         for(j=0; j<N; j++){
-            fprintf(fp,",%d ",R[i][j]);
+            fprintf(fp,",%d ",C[i][j]);
         }
         fprintf(fp,"\n%d",i+1);
     }
@@ -75,22 +77,36 @@ int main(int argc, char **argv){
     char* fileA = argv[1];
     char* fileB = argv[2];
     int N = atoi(argv[3]);
-    int H = atoi(argv[4]);    
+    int H = atoi(argv[4]);
 
-    // Read matrix A and B
+    // Create, read & print matrix A
+    int** A = createMatrix(N);
     readMatrix(fileA, A, N);
-    readMatrix(fileB, B, N);
-
-    // Print matrix A and B
     printMatrix(A, N);
+
+    // Create, read & print matrix B
+    int** B = createMatrix(N);
+    readMatrix(fileB, B, N);
     printMatrix(B, N);
     
-    // Obtain and print matrix C
-    multiplyMatrix(A, B, C);      
+    // Create C, obtain & print marix C
+    int** C = createMatrix(N);
+
+    #pragma omp parallel num_threads(H)
+    {
+        int tn = omp_get_thread_num();
+        int ini = (int)(N/H)*(tn);
+        int fin = (int)(N/H)+ini;
+        multiplyMatrix(A, B, C, N, ini, fin);
+    }
+
     printMatrix(C, N);
 
     // Write the matrix
     writeMatrix("result.csv", C, N);
+    free(A);
+    free(B);
+    free(C);
     
     return 0;
 }
