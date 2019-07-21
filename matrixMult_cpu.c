0e8de33e01a0314e7a1f6526eb99a2ec9099ec26
Andres Forero <afforeroc@unal.edu.co>
2019-07-21 00:18:43 -0500

    First commit

---
+++ matrixMult_cpu.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <omp.h>
+#include <stdio.h>
+#include <string.h>
+
+// Global variables
+int A[1024][1024];
+int B[1024][1024];
+int C[1024][1024];
+
+void readMatrix(char *filename, int M[][1024], int N){
+    FILE *fstream = fopen(filename, "r");
+    if(fstream == NULL){
+        printf("\n file opening failed ");
+        return;
+    }
+    char *record,*line;
+    char buffer[1024];
+    int i=0,j=0;
+    while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL){
+        j = 0;
+        record = strtok(line,",");
+        while(record != NULL){
+            M[i][j++] = atoi(record);
+            record = strtok(NULL,",");
+        }
+        ++i;
+    }
+}
+
+void printMatrix(int M[][1024], int N){
+    // print matrix for testing
+    int i;
+    int j;
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            printf("%d ",M[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void multiplyMatrix(int A[][1024], int B[][1024], int C[][1024]) 
+{ 
+    int i, j, k; 
+    for (i = 0; i < 1024; i++) { 
+        for (j = 0; j < 1024; j++) { 
+            C[i][j] = 0; 
+            for (k = 0; k < 1024; k++) 
+                C[i][j] += A[i][k]*B[k][j]; 
+        } 
+    } 
+}
+
+void writeMatrix(char *filename, int R[][1024], int N){
+    FILE *fp;
+    int i,j;
+    fp=fopen(filename,"w+");
+    for(i=0 ; i<N; i++){
+        for(j=0; j<N; j++){
+            fprintf(fp,",%d ",R[i][j]);
+        }
+        fprintf(fp,"\n%d",i+1);
+    }
+    fclose(fp);
+}
+
+int main(int argc, char **argv){
+    // Arguments
+    if ( argc !=  5){
+        printf("usage: ./matrixMult MatA.csv MatB.csv N H\n");
+        return -1;
+    }
+    char* fileA = argv[1];
+    char* fileB = argv[2];
+    int N = atoi(argv[3]);
+    int H = atoi(argv[4]);    
+
+    // Read matrix A and B
+    readMatrix(fileA, A, N);
+    readMatrix(fileB, B, N);
+
+    // Print matrix A and B
+    printMatrix(A, N);
+    printMatrix(B, N);
+    
+    // Obtain and print matrix C
+    multiplyMatrix(A, B, C);      
+    printMatrix(C, N);
+
+    // Write the matrix
+    writeMatrix("result.csv", C, N);
+    
+    return 0;
+}
